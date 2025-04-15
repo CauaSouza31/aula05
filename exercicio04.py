e11e76e73f5ba19f8e64eca3fc80df56aaea73a0
Cauã Souza <cauasouza3112@gmail.com>
2025-04-15 15:38:56 -0300

    exercicio de receber doi numeros e /

---
+++ exercicio04.py
@@ -0,0 +1,9 @@
+valor=0
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite um valor: "))
+while n2 ==0:
+    n2=int(input("Dgite uma valor valido: "))
+resultado=n1/n2
+print(resultado)
+
+
